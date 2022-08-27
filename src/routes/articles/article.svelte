c510883be3c4801e5c7d098ac0953fe40342bd80
voldari <isaacharper@gmail.com>
2022-08-27 13:25:02 -0700

    update blog system to be expandable while making use of mdsvex to make writing a more user friendly process

---
+++ src/routes/articles/article.svelte
@@ -0,0 +1,20 @@
+<!-- article.svelte -->
+<!-- template for articles using mdsvex -->
+<script>
+	import BackButton from './../../lib/elements/BackButton.svelte';
+	import Page from './../../lib/elements/Page.svelte';
+    export let title
+    export let date
+</script>
+    
+
+
+<Page>
+  <svelte:fragment  slot="title">{title}</svelte:fragment>
+  <svelte:fragment  slot="intro">{date}</svelte:fragment>
+
+  <svelte:fragment slot="contents">
+    <slot />
+    <BackButton/>
+  </svelte:fragment>
+</Page>
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,29 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import sveltePreprocess from 'svelte-preprocess';
+
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
 
 	kit: {
 		adapter: adapter()
-	}
+	},
+
+	extensions: ['.svelte', '.md'],
+
+	preprocess: [
+		sveltePreprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			layout: { 
+				articles: 'src/routes/articles/article.svelte'
+			}
+		})
+	],
 };
 
 export default config;
