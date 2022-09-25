e86083861f79678813db3ec4c1a63ebff6ce05ac
voldari <isaacharper@gmail.com>
2022-09-24 17:29:58 -0700

    adding vercel stats

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
 <script>
-import Header from '$lib/elements/Header.svelte';
-import '/node_modules/modern-normalize/modern-normalize.css';
+	import Header from '$lib/elements/Header.svelte';
+	import '/node_modules/modern-normalize/modern-normalize.css';
+	import { webVitals } from '$lib/vitals'
+	import { browser } from '$app/env';
+  	import { page } from '$app/stores';
+
+
+	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+	
+	$: if (browser && analyticsId) {
+		webVitals({
+			path: $page.url.pathname,
+	      	params: $page.params,
+	        analyticsId
+	    })
+	}
+
 </script>
 
 <Header/>
