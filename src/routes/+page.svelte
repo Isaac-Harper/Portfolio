0474ff1bf65825f960ca7d7aa47798f4858e422a
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-11-27 18:01:23 -0700

    src

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 	
 	initialize({ cloud_name: 'dn0dhuunu' })
 	
-	let portrait = 'portrait'
+	const src = 'portrait'
     let portrait2 = '/images/portrait.jpg'
 
     const now = new Date();
@@ -25,7 +25,7 @@
 
     <svelte:fragment slot="contents">
         <h2 class="highlight">About Me</h2>
-        <img use:image={{ portrait, bind: true, lazy: true}} alt="Portait"  class="img"/>
+        <img use:image={{ src, bind: true, lazy: true}} alt="Portait"  class="img"/>
         <p>I'm Isaac (He/Him/They). I am a <span class="highlight" title={string}>22</span> year old Arizonan who is interested in a lot of things.  A couple of the things I'm interested in that may be featured on this site include web development, hobby electronics, worldbuilding, CAD, coffee brewing, video games, backpacking, and fishing. </p>
 
         <p>Take a look around to see the new stuff I've been up to. Also feel free to get in touch at <a href="mailto:isaacharperdev@gmail.com" class="highlight">IsaacHarperDev@gmail.com</a></p>
